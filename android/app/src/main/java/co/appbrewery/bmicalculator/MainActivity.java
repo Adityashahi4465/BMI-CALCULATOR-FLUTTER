2eba69eaa66e40a4d5ef91b71e56961baf56e007
Adityashahi4465 <97585273+Adityashahi4465@users.noreply.github.com>
2022-06-03 02:46:30 +0530

    Add files via upload

---
+++ android/app/src/main/java/co/appbrewery/bmicalculator/MainActivity.java
@@ -0,0 +1,12 @@
+package co.appbrewery.bmicalculator;
+
+
++import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
